ae22db10f1d5c3081f11b66f14a00f6947afab4d
HuiYang1997 <59648587+HuiYang1997@users.noreply.github.com>
2022-10-10 15:44:35 +0200

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,10 @@ subsumption_pairs =[['7', '6'], ['7', '5']]
 
 # traing_complete set
 result, s_a_pre, goal_id = T.trace_inference_rules(subsumption_pairs)
-# result is dictionary: conclusion->[premise set 1, premise set 2, ...]
+# result is a dictionary of the form:
+#
+#         {conclusion:[premise set 1, premise set 2, ...], ... }
+#
 # s_a_pre: answer literals
 # id of queries in subsumption_pairs
 # Note that if there are several subsumptions, then the result will be integrated all together
